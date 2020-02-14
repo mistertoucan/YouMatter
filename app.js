36abc0bcf3dd901991065bd59e4be943e962980c
anthonylekan <anthonylekan25@gmail.com>
2020-02-14 08:28:29 -0500

    shifted all webhook code from app.js to webhooks/router

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ require('./db');
 const express = require('express');
 const { urlencoded } = require('body-parser');
 
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
-
-const OperatorController = require('./operator/controller');
-
 const app = express();
 
 app.use(urlencoded({ extended: true }));
@@ -16,51 +12,6 @@ app.set("view engine", "ejs");
 app.use((req, res, next) => {
     res.type('text/xml');
     next();
-});
-
-app.post('/webhooks/voice', (req, res) => {
-    const { From } = ctx.body;
-    const response = new VoiceResponse();
-    response.say("You Matter! And we're here to make sure you know so.", { voice: "female" });
-
-    OperatorController.get_available_operator(function(err, operator) {
-        if(!err && operator) {
-            console.log(`> Info: Dialing ${operator.phone_number}`);
-            response.say(`You are being redirect to talk to ${operator.name.split(' ')[0]}.`);
-            response.dial(operator.phone_number, { action: '/end' });
-            res.send(response.toString());
-
-            OperatorController.send_sms(operator, From);
-            OperatorController.mark_operator_unavailable(operator.phone_number);
-        } else {
-            console.log('> Info: Can\'t connect find available operator @ webhooks/voice', err);
-            const dial = response.dial(operator.phone_number);
-            dial.queue({
-                url: './queue'
-            });
-
-            res.send(response.toString());
-        }
-    });
-
-});
-
-app.post('/webhooks/voice/queue', (req, res) => {
-    const response = new VoiceResponse();
-    response.say("Please wait while we connect you...");
-
-    res.send(response.toString());
-});
-
-app.post('/webhooks/voice/end', (req, res) => {
-    const { DialSid } = res.status;
-
-    OperatorController.get_dail_to(DialSid, function(call) {
-        const { to } = call;
-
-        OperatorController.mark_operator_available(to);
-        ctx.status = 200;
-    });
 });
 
 app.post("*", (req, res) => {
---
+++ webhooks/router.js
@@ -0,0 +1,53 @@
+const express = require('express');
+
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+const OperatorController = require('./operator/controller');
+
+const router = express.Router();
+
+router.post('/webhooks/voice', (req, res) => {
+    const { From } = ctx.body;
+    const response = new VoiceResponse();
+    response.say("You Matter! And we're here to make sure you know so.", { voice: "female" });
+
+    OperatorController.get_available_operator(function(err, operator) {
+        if(!err && operator) {
+            console.log(`> Info: Dialing ${operator.phone_number}`);
+            response.say(`You are being redirect to talk to ${operator.name.split(' ')[0]}.`);
+            response.dial(operator.phone_number, { action: '/end' });
+            res.send(response.toString());
+
+            OperatorController.send_sms(operator, From);
+            OperatorController.mark_operator_unavailable(operator.phone_number);
+        } else {
+            console.log('> Info: Can\'t connect find available operator @ webhooks/voice', err);
+            const dial = response.dial(operator.phone_number);
+            dial.queue({
+                url: './queue'
+            });
+
+            res.send(response.toString());
+        }
+    });
+
+});
+
+router.post('/webhooks/voice/queue', (req, res) => {
+    const response = new VoiceResponse();
+    response.say("Please wait while we connect you...");
+
+    res.send(response.toString());
+});
+
+router.post('/webhooks/voice/end', (req, res) => {
+    const { DialSid } = res.status;
+
+    OperatorController.get_dail_to(DialSid, function(call) {
+        const { to } = call;
+
+        OperatorController.mark_operator_available(to);
+        ctx.status = 200;
+    });
+});
+
+module.exports = router;
